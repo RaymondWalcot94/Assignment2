af92bd6dfbaa034602f98b842a80dd5de9148d77
RaymondWalcot94 <raymondwalcott94@gmail.com>
2017-02-08 02:27:58 -0500

    Update13

---
+++ 4Structures3.pl
@@ -0,0 +1,3 @@
+common_unique_elements([], _, []). /* base case where list is empty */
+common_unique_elements([Head|End], L1, L2) :- member(Head, L1), !, L2 = [Head|End2], /* checks if it is a member also prevents unnecessary back tracking */
+common_unique_elements(End, L1, End2). /* defined fact */
